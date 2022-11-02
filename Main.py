a72df15f5d637f20931ae5718d20ecaf6c03b679
nikhilt2412 <110010400+nikhilt2412@users.noreply.github.com>
2022-11-02 21:19:35 +0530

    10.5

--- Main.py
+++ Main.py
@@ -3,51 +3,40 @@ class BinaryTreeNode:
         self.data = data
         self.left_child = None
         self.right_child = None
-
-
+    
 def insert(root, new_value) -> BinaryTreeNode:
-    """If binary search tree is empty, make a new node, declare it as root and return the root.
-        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
-        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
-        Finally, return the root.
-        """
-    # Write your code here
-    p = BinaryTreeNode(new_value)
-    
-    if root == None:
-        root = p
-    
-    else:
-        temp = root
-        while temp.right_child != None or temp.left_child != None:
-            if temp.data > new_value:
-                temp = temp.left_child
+    if (root == None):         
+        root = BinaryTreeNode(new_value)
+        return root
+    else:            
+        if root.data > new_value:
+            if root.left_child is None:
+                new_node = BinaryTreeNode(new_value)
+                root.left_child = new_node
             else:
-                temp = temp.right_child
-        if temp.data > new_value:
-                temp.left_child = p
-        else:
-                temp.right_child = p
-
-    return root
-
-def inorder(self, root) -> None:
-    # Write your code here
-        res = []
-        
-        if root:
-            res = self.inorder(root.left)
-            res.append(root.data)
-            res = res + self.inorder(root.right)
-            return res
-        
+                insert(root.left_child,new_value)
+        else:               
+            if root.right_child is None:
+                new_node = BinaryTreeNode(new_value)
+                root.right_child = new_node
+            else:
+                insert(root.right_child,new_value)
+def inorder(root) -> None:
+    if root:
+        inorder(root.left_child)
+        print(root.data, end = " ")
+        inorder(root.right_child)
+    
 def preorder(root) -> None:
-    # Write your code here
-
-
+    if root:
+        print(root.data, end = " ")
+        preorder(root.left_child)
+        preorder(root.right_child)
 def postorder(root) -> None:
-    # Write your code here
-
+    if root:
+        postorder(root.left_child)
+        postorder(root.right_child)
+        print(root.data, end = " ")
 
 # Do not change the following code
 input_data = input()
